36e3732a95d770c1bfbe50996f7f3a881cb39f11
Abe Ost <abraham.ospina416@pascualbravo.edu.co>
2024-09-19 16:47:37 -0500

    Add project files.

---
+++ GestionAcademica/Asignatura.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAcademica
+{
+    public class Asignatura
+    {
+        //propiedades
+        public string Nombre { get; set; }
+        public string Profesor { get; set; }
+        public string Creditos { get; set; }
+        public int Estudiantes { get; set; }
+        public Asignatura(string nombre, string profesor, string creditos, int estudiantes)
+        {
+            Nombre = nombre;
+            Profesor = profesor;
+            Creditos = creditos;
+            Estudiantes = estudiantes;
+        }
+        public Asignatura() //Constructor por defecto
+        {
+        }
+        public void IngresarAsignatura()
+        {
+            try
+            {
+                Console.Write("Ingrese el nombre de la asignatura ");
+                Nombre = Console.ReadLine();
+
+                Console.Write("Ingrese el nombre del profesor: ");
+                Profesor = Console.ReadLine();
+
+                Console.Write("Ingrese el numero de creditos ");
+                Creditos = Console.ReadLine();
+
+                Console.Write("Ingresa el nombre del estudiante: ");
+                Estudiantes = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error en el formato de entrada: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocurrió un error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ GestionAcademica/Universidad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestionAcademica
+{
+    public class Universidad
+    {
+        public List<Asignatura> ListaAsignatura { get; set; }  //Creo una lista basada en los parametros de Asignatura
+        public Universidad()
+        {
+            ListaAsignatura = new List<Asignatura>();
+        }
+        public void InscribirEstudiante(Asignatura listaAsignatura)
+        {
+            if(listaAsignatura != null)
+            {
+               ListaAsignatura.Add(listaAsignatura);
+            }
+            else
+            {
+                Console.WriteLine("El campo no puede estar vacio");
+            }
+        }
+
+    }
+}
